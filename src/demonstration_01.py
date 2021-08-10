e791f16e945743da0d733eb7add753320401f45a
Kedasha Kerr <kedashakerr@northwesternmutual.com>
2021-08-10 11:04:34 -0500

    done

--- src/demonstration_01.py
+++ src/demonstration_01.py
@@ -1,6 +1,6 @@
 """
-Given an array of integers `nums` and an integer `target`, return the indices
-of the two numbers that add up to the `target`.
+Given an array of integers `nums` and an integer `target`, 
+return the indices of the two numbers that add up to the `target`.
 
 Examples:
 
@@ -12,7 +12,33 @@ Notes:
 - Each input will have only one solution.
 - You may not use the same element twice.
 - You can return the answer in any order.
+
+todo:
+- find the two nums in the arr that equal the target
+- return the index of those nums
+
+explanation: https://levelup.gitconnected.com/two-sum-interview-problem-in-python-c1d84b029d35
 """
 def two_sum(nums, target):
-    # Your code here
+    # method 1 - O(n^2)
+    for num in range(len(nums)):
+        for next_num in range(num+1, len(nums)):
+            if nums[num] + nums[next_num] == target:
+                return [num, next_num]
+
+    #method 2 - O(n)
+    new_dict = {}
+
+    for num in range(len(nums)):
+        next_num = target - nums[num]
+        if(next_num in new_dict.keys()):
+            next_num = nums.index(next_num)
+            if(num != next_num):
+                return sorted([num, next_num])
+        
+        new_dict.update({nums[num]: num})
+
+
+print(two_sum(nums = [2,5,9,13], target = 7))
+print(two_sum(nums = [4,3,5], target = 8))
 
